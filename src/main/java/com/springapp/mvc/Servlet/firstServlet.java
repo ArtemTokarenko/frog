430bcfeeae2e3e15bf56bbba4b7cd083d745bff5
ArtemTokarenko <skymennn@gmail.com>
2016-06-08 21:35:44 +0300

    1

--- src/main/java/com/springapp/mvc/Servlet/firstServlet.java
+++ src/main/java/com/springapp/mvc/Servlet/firstServlet.java
@@ -1,26 +1,48 @@
 package com.springapp.mvc.Servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /**
  * Created by Ультрамар on 07.06.2016.
  */
 public class firstServlet extends HttpServlet{
+int count;
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+        config.getInitParameter("init1");
+    }
+
+
     protected void processRequest (HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException{
         response.setContentType("text/html;charset=UTF8");
         PrintWriter out = response.getWriter();
+
+        Enumeration en = request.getParameterNames();
+
+
+        while (en.hasMoreElements()){
+            String p = en.nextElement().toString();
+            out.println("<h1>"+ p+" = "+request.getParameter(p)+"</h1>");
+        }
+        count++;
+        request.getSession().setAttribute("count",count);
         try{
             out.println("<html>");
             out.println("<head>");
 
             out.println("<img src='http://media.emirates247.com/images/2014/01/gvDSC_9387.JPG'");
 
+
+            out.println("<h1>"+ request.getSession().getAttribute("count")+"</h1>");
 
             out.println("<title>Стили</title>");
             out.println("<link rel='stylesheet' type='text/css' href='mysite.css'>");
@@ -34,8 +56,21 @@ throws ServletException, IOException{
             out.println("<body>");
 
 
-            out.println("<h1>вася пупкин "+ request.getContextPath()+"</h1>");
+//            out.println("<h1>getContextPath( "+ request.getContextPath()+"</h1>");
+//            out.println("<h1>getAuthType() "+ request.getAuthType()+"</h1>");
+//            out.println("<h1>getHeader(p1) "+ request.getHeader("p1")+"</h1>");
+//            out.println("<h1>getPathInfo() "+ request.getPathInfo()+"</h1>");
+//            out.println("<h1>getPathTranslated() "+ request.getPathTranslated()+"</h1>");
+//            out.println("<h1>getQueryString() "+ request.getQueryString()+"</h1>");
+//            out.println("<h1>getMethod() "+ request.getMethod()+"</h1>");
+//            out.println("<h1>getRequestedSessionId() "+ request.getRequestedSessionId()+"</h1>");
+            out.println("<h1>"+ request.getRequestURI()+"</h1>");
+//            out.println("<h1>getServletPath() "+ request.getServletPath()+"</h1>");
+//            out.println("<h1>getCharacterEncoding() "+ request.getCharacterEncoding()+"</h1>");
 
+
+//            out.println("<h1>параметр1 = " + request.getParameter("p1") + "</h1>");
+//            out.println("<h1>параметр2 = "+ request.getParameter("p2")+"</h1>");
 
             out.println("<img src='WEB-INF/images/w.jpg/'");
 
